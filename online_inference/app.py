469e410a946d3363cd64650e6bc79ebbd4137026
UlianaBespalova <bespalovau@yandex.ru>
2024-12-10 15:25:28 +0300

    Убрала лишний код

--- online_inference/app.py
+++ online_inference/app.py
@@ -65,22 +65,6 @@ def predict(request: InputData):
     return OutputData(predicted_values=y_pred) # Возвращаем результат
 
 
-@app.get("/is_ready")
-def is_ready():
-    if model_lgbm:
-        status = 200
-        message = "Model is ready ^-^"
-    else:
-        status = 500
-        message = "Model not found :("
-    return JSONResponse(
-        status_code = status,
-        content = jsonable_encoder({"Message": message})
-    )
-
-
-
-
 # Функция, срабатывающая при ошибке
 # (если данные в post запросе имеют неправильный формат)
 @app.exception_handler(RequestValidationError)
